9fb397ea655f1e344520ea7dd5db5e61db74ad9e
Thong Zi Qi <ziqi.thong@gmail.com>
2024-10-10 13:46:50 +0800

    fix - tailwind not apply style to next due to spaces in tailwind config

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,15 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container
-                    padding-container relative z-30 py-5">
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         NavBar
       </Link>
       <Link href="/gallery">
-        Image Galler
+        Image Gallery
       </Link>
     </nav>
   )
 }
 
-export default Navbar;  
+export default Navbar;
