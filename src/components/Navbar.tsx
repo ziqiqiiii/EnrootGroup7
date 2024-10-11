60f55372fa758b177c080c851b3878ff48bd1a70
Thong Zi Qi <ziqi.thong@gmail.com>
2024-10-11 17:59:01 +0800

    fix - update sign up link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = () => {
 			title="Sign Up Now"
 			icon="/add-user.png"
 			variant="btn_white shadow-2xl shadow-green-500/40"
-			link = "https://forms.office.com/Pages/DesignPageV2.aspx?origin=NeoPortalPage&subpage=design&id=drd2NJDpck-5UGJImDFiPbCyV33gL2RDgf4IvAKYHrhUM1BIVEhVQ1g1R0tDUVZaWE81WVdGQVFBNi4u&analysis=false"
+			link = "https://forms.office.com/Pages/DesignPageV2.aspx?subpage=design&token=a7300996ccaf4d338df9b36b29961891&id=drd2NJDpck-5UGJImDFiPV1Dk1XmGrxMvE1CPIuoPlRUQjVPTjhLTFgwVFVDMEdTNkJKMVdZWUdVOSQlQCN0PWcu"
 		/>
 	  </div>
 	  <div className="lg:hidden relative">
