54d6b3eb3e95c0aa3454d3489c80e4afff534065
Thong Zi Qi <ziqi.thong@gmail.com>
2024-10-11 17:38:24 +0800

    feat - done footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="text-white fixed w-full p-10 justify-between items-center flexBetween bottom-3">
+      <div className="footer-gradient flexCenter w-full h-16 rounded-md">
+        <p>&copy; Enroot Group 7 Soft Toy Workshop</p>
+      </div>
+    </footer>
   )
 }
 
