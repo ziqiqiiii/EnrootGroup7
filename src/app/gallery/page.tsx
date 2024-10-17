3361dd5090603169bd208bda95ba8100208be65a
Jiahao <yaojiahao048@gmail.com>
2024-10-17 11:15:27 +0800

    adding in images

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,8 +1,14 @@
+
 export default function Gallery() {
-  
   return (
     <div className="space-y-20 flex flex-col items-center justify-center min-h-screen mt-[-150px]">
       <h1 className="mt-4 text-lg text-gray-700">Gallery</h1>
+      <h1>
+        
+      </h1>
     </div>
   );
 }
+
+// https://sutdapac.sharepoint.com/:f:/s/enrootgroup7/EhBRr4jehAxAj9nPGXEyzggBj5kzvnz_EEb8QUTjF9rPCA?e=ewZs5E
+
