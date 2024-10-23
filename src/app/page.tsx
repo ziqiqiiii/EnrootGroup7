b72fa6910476bdd1b059779cb37fb61fb6ba0a7d
Thong Zi Qi <ziqi.thong@gmail.com>
2024-10-24 00:00:25 +0800

    refactor : refactor the code for event details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,6 @@ export default function Home() {
   return (
     <div className="space-y-20 flex flex-col items-center justify-center min-h-screen mt-[-150px]">
       <EventDetails />
-      <ClientWrapper>
-        <Countdown targetDate={launchDate} />
-      </ClientWrapper>
     </div>
   );
 }
--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import ClientWrapper from "@/components/ClientWrapper";
+import Countdown from "@/components/Countdown";
+
+const launchDate = '2024-11-20T19:00:00';
 
 const EventDetails = () => {
     return (
-        <div className="text-center max-w-xl mx-auto">
-          <h1 className="text-4xl font-bold">EnROOT Group 7 Soft Toy Workshop</h1>
-          <p className="mt-4 text-lg text-gray-700">
-          Who does not love cute plushies?? Join us as we make adorable cats, snowmen, and koalas out of socks!! Come down to learn a new skill and relax while making new friends.  
+        <div className="flex flex-col flexCenter text-center max-w-5xl mx-auto font-serif">
+          <h1 className="text-5xl font-bold py-3 italic ">EnROOT Group 7 Soft Toy Workshop</h1>
+          <ClientWrapper>
+            <Countdown targetDate={launchDate} />
+          </ClientWrapper>
+          <p className="text-lg text-gray-700 max-w-xl">
+            Who does not love cute plushies?? Join us as we make adorable cats, snowmen, and koalas out of socks!! Come down to learn a new skill and relax while making new friends.  
           </p>
           <p className="mt-2 text-gray-500">
             Location: Think Tank 26 | Day: 20 November | Time: 7:00 PM SGT
