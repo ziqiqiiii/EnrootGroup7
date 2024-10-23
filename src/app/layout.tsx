2d9f91e20818508e0f5b2db764e8d808fd5c706d
Thong Zi Qi <ziqi.thong@gmail.com>
2024-10-24 00:03:35 +0800

    chore : remove redundant lines of code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Soft Toy Workshop",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 import EventDetails from "@/components/EventDetails";
-import ClientWrapper from "@/components/ClientWrapper";
-import Countdown from "@/components/Countdown";
 
 export default function Home() {
-  const launchDate = '2024-11-20T19:00:00';
-
   return (
     <div className="space-y-20 flex flex-col items-center justify-center min-h-screen mt-[-150px]">
       <EventDetails />
