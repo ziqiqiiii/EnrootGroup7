ac70b65c345b9e9919b2fe915b21e3e88fefcde9
Thong Zi Qi <ziqi.thong@gmail.com>
2024-10-27 22:33:27 +0800

    chore - removed redundant file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import EventDetails from "@/components/EventDetails";
-import Carousel from "@/components/Carousel";
 import EventDescription from "@/components/EventDescription";
 
 export default function Home() {
   return (
     <div className="flex flex-col flexCenter items-center justify-center lg:py-14 py-8 overflow-hidden font-serif">
       <EventDetails />
-      {/* <Carousel /> */}
       <EventDescription />
     </div>
   );
