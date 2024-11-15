a3c058a05aa0e90cee060ab68e41cbb4b1f85dfe
Jiahao <yaojiahao048@gmail.com>
2024-11-15 10:49:09 +0800

    updating the the workshope name and hide gallery

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -4,7 +4,7 @@ import Button from './Button';
 function Description() {
   return (
     <div className='flex flex-col lg:w-max-[130px] lg:h-[600px] lg:py-36 py-16 px-8'>
-        <div className='text-green-950 lg:text-4xl text-3xl'>Soft Toy Workshop</div>
+        <div className='text-green-950 lg:text-4xl text-3xl'>Plushie Making Workshop</div>
         <div className='text-light-brown-green text-sm'>by EnROOT Group7</div>
         <p className="text-base lg:text-lg sm:text-xs max-w-xl mt-4">
             Who does not love cute plushies?? Join us as we make adorable cats, snowmen, and koalas out of socks!! 
